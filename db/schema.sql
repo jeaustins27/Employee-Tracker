066394516fc513d87076bc25136603eca053fd5a
Jeaustin <JeaustinS27@gmail.com>
2023-03-08 12:19:56 -0500

    created table for roles in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE departments (
     id INT NOT NUll AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)  NOT NULL
 );
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL  NOT NULL,
+    department_id INT  NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);

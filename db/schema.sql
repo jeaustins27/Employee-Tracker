14b458bb26f04d9dcd3ccb5d4233f2d44a93c4d1
Jeaustin <JeaustinS27@gmail.com>
2023-03-08 20:01:27 -0500

    created table for employees in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE roles (
     FOREIGN KEY (department_id)
     REFERENCES departments(id)
 );
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT  NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    );

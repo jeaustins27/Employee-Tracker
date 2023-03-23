ec9a0c26bd38f0eccc2b6de94cbc7a24cf5f6cf5
Jeaustin <JeaustinS27@gmail.com>
2023-03-22 22:09:11 -0400

    added database code to index.js, adjusted schema.sql, and added code to query.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,33 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employee_db;
+USE employees_db;
 
-CREATE TABLE departments (
-    id INT NOT NUll AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)  NOT NULL
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL  NOT NULL,
-    department_id INT  NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    salary DECIMAL(10,2) NOT NULL,
+    dept_id INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT  NOT NULL,
+    role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
-    );
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE NO ACTION
+);
